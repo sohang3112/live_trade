a7f41af25580b41857e174322a4ad1ed46f4602f
Sohang Chopra <sohangchopra@gmail.com>
2021-12-20 08:54:25 +0530

    Created (and called) function get_request_token
    Process of getting Zerodha request token from Kite login url via
    Selenium is now wrapped in a function and called. Previously, the code
    was used directly.

--- option_chain.py
+++ option_chain.py
@@ -10,6 +10,19 @@ from urllib.parse import parse_qs, urlparse
 
 chrome_path = ChromeDriverManager().install()
 
+def get_request_token(kite_url: str, user_id: str, password: str, totp: str) -> str:
+    with Chrome(chrome_path) as driver:
+        wait = WebDriverWait(driver, 10)
+        driver.get(kite_url)
+        wait.until(EC.presence_of_element_located((By.ID, 'userid'))).send_keys(user_id)
+        driver.find_element(By.ID, 'password').send_keys(password)
+        driver.find_element(By.CSS_SELECTOR, '.login-form button[type="submit"]').click()
+        wait.until(EC.presence_of_element_located((By.ID, 'totp'))).send_keys(totp)
+        driver.find_element(By.CSS_SELECTOR, '.login-form button[type="submit"]').click()
+        wait.until(lambda driver: 'request_token' in driver.current_url)
+        token_url = driver.current_url
+    return parse_qs(urlparse(token_url).query)['request_token'][0]
+
 config = ConfigParser()
 config.read('config.ini')
 zerodha = config['zerodha']
@@ -17,20 +30,8 @@ zerodha = config['zerodha']
 totp_auth = TOTP(zerodha['totp_key'])
 kite = KiteConnect(zerodha['api_key'])
 
-totp = totp_auth.now()
-
-with Chrome(chrome_path) as driver:
-    wait = WebDriverWait(driver, 10)
-    driver.get(kite.login_url())
-    wait.until(EC.presence_of_element_located((By.ID, 'userid'))).send_keys(zerodha['user_id'])
-    driver.find_element(By.ID, 'password').send_keys(zerodha['password'])
-    driver.find_element(By.CSS_SELECTOR, '.login-form button[type="submit"]').click()
-    wait.until(EC.presence_of_element_located((By.ID, 'totp'))).send_keys(totp)
-    driver.find_element(By.CSS_SELECTOR, '.login-form button[type="submit"]').click()
-    wait.until(lambda driver: 'request_token' in driver.current_url)
-    url = driver.current_url
-
-request_token = parse_qs(urlparse(url).query)['request_token'][0]
+request_token = get_request_token(
+    kite.login_url(), user_id=zerodha['user_id'], password=zerodha['password'], totp=totp_auth.now())
 
 data = kite.generate_session(request_token, api_secret=zerodha['api_secret'])
 kite.set_access_token(data['access_token'])
